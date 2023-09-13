61e9c89757353c63b96dda3356def0221bf9aa7d
ErikaFlores <foglzerika@gmail.com>
2023-09-13 17:37:41 -0600

    styles | Change display container to flex

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,33 +5,38 @@ import { useAxios } from '../customHooks/useAxios';
 import CircularProgress from '@mui/material/CircularProgress';
 import { Types } from '../context/blogReducer';
 import { useEffect } from 'react';
+import { Stack } from '@mui/material';
 const Home = () => {
 
-  const { state, dispatch} = useAppContext();
-  
-  const {data, isLoading} = useAxios('/posts');
+  const { state, dispatch } = useAppContext();
 
-  useEffect(()=>{
+  const { data, isLoading } = useAxios('/posts');
+
+  useEffect(() => {
     dispatch({
-           type: Types.SET_POSTS,
-           payload: data
+      type: Types.SET_POSTS,
+      payload: data
     })
-  },[data])
-  
+  }, [data])
+
   return (
-    <Box sx={{ gridArea: 'main', bgcolor: 'secondary.main' }}>
+    <Box sx={{ gridArea: 'main', bgcolor: 'secondary.main' , padding:'5rem'}}>
+
       <Box>
         <h2>Blog post</h2>
       </Box>
-      <Box>
+      <Stack
+        sx={{ gridArea: 'main', bgcolor: 'secondary.main' }}
+        direction={{ xs: 'column', sm: 'row' }}
+        spacing={{ xs: 1, sm: 2, md: 4 }}
+      >
         {
           isLoading && (<CircularProgress />)
         }
         {
-          state.posts.length === 0 ? 'Sin Posts' : <ListPosts data={data}/>
+          state.posts.length === 0 ? 'Sin Posts' : <ListPosts data={data} />
         }
-      </Box>
-
+      </Stack>
     </Box>
   )
 }
